4e126ed565826eb52c27de4923f09c0459f904c3
Basel Ayman <baselaymansalah@gmail.com>
2022-07-30 23:59:52 +0200

    Time: 77 ms (83.46%), Space: 14.8 MB (43.77%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # nums[:] = sorted(list(set(nums)))
-        k = 1
-        for i in range(len(nums)-1):
-            if nums[i] != nums[i+1]:
-                nums[k] = nums[i+1]
-                k += 1
-        return k
+        nums[:] = sorted(list(set(nums)))
+        
+        return len(nums)
