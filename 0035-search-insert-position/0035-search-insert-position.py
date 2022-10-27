94adb00be7481f4e31d4417e2b7c619ad8598d86
Basel Ayman <baselaymansalah@gmail.com>
2022-10-28 00:38:53 +0200

    Time: 84 ms (10.52%), Space: 14.1 MB (52.81%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # must write an algorithm with "O(log n)" runtime complexity => binary search
+        
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            mid = (low + high) // 2 # integer
+            guess = nums[mid]
+            
+            if guess == target:
+                return mid
+            if guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1 
+        
+        return low
+        
